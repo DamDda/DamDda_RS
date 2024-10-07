66931a545ac6537849049815746ac7c9c99b17bf
taeuk0210 <157875673+taeuk0210@users.noreply.github.com>
2024-10-07 21:02:20 +0900

    fix: update route

--- src/routes.py
+++ src/routes.py
@@ -4,6 +4,6 @@ from .TFIDF_Model import TFIDF_Model
 main_bp = Blueprint("main", __name__)
 model = TFIDF_Model()
 
-@main_bp.route("/api/recommend/<member_id>", methods=["GET"])
+@main_bp.route("/api/recommend/<int:member_id>", methods=["GET"])
 def getRecommendation(member_id):
     return model.getRecommendationFromMemberId(member_id)
