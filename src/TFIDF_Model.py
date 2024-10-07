2e8234f11e2848d49af931a7fddb9ca394026715
taeuk0210 <157875673+taeuk0210@users.noreply.github.com>
2024-10-07 16:43:11 +0900

    Update TFIDF_Model.py

--- src/TFIDF_Model.py
+++ src/TFIDF_Model.py
@@ -7,7 +7,7 @@ import numpy as np
 import pandas as pd
 
 from sklearn.feature_extraction.text import TfidfVectorizer
-from application import DataSource
+from ..application import DataSource
 
 class TFIDF_Model:
     def __init__(self):
